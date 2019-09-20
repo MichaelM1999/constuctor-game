a8079cd376761bc73a086db76c453ff79b89ba3d
michael mariea <mariea.michael@gmail.com>
2019-09-19 18:16:22 -0700

    added some questions

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ class Player {
         this.health = 100;
         this.maxHealth = 100;
         this.enemyHealth = 20;
+        this.enemyMaxHealth = 20;
         this.enemydamage = 10;
     }
     levelUp() {
@@ -17,7 +18,8 @@ class Player {
         this.damage += 2;
         this.maxHealth += 10;
         this.health = this.maxHealth;
-        this.enemyHealth += 10;
+        this.enemyMaxHealth += 10;
+        this.enemyHealth = this.enemyMaxHealth;
         this.enemydamage += 2;
         console.log(this.name + ", " + this.title + " is now level: " + this.level + " You have been fully healed and your new max health is: " + this.maxHealth + " and your damage is now: " + this.damage);
     }
@@ -26,15 +28,13 @@ class Player {
     }
     hurt() {
         this.health -= this.enemydamage;
-        console.log("you have been hit for 10 health you now have " + this.health)
+        console.log("you have been hit for " + this.enemydamage + " health you now have " + this.health)
         if (this.health <= 0)
         this.dead();
     }
     enemy() {
-        console.log("An enemy has appeared he has " + this.enemyHealth + "health");
+        console.log("An enemy has appeared he has " + this.enemyHealth + " health");
         while (this.enemyHealth > 0 || this.health >= 0){
-        this.hurt();
-        this.attack();
         } 
     }
     attack() {
@@ -46,6 +46,21 @@ class Player {
         }
     }
 }
-const mike = new Player("mike", "the cool guy");
+// const mike = new Player("mike", "the cool guy");
 
-mike.enemy();
+inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "what is your name"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "what is your title? ex. 'The Great'"
+    }
+  ]).then((name) => {
+    const user = new Player(name.name, name.title);
+    console.log("These are your stats:");
+    console.log(user);
+  })
